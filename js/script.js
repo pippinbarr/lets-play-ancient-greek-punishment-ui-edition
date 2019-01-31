bb9a27571df4d0e9a9f07d3dbe0136f4f53ba4bd
Pippin Barr <pippin.barr@gmail.com>
2019-01-31 07:49:07 -0500

    Added first draft of Zeno
    ? Seems like just doing ultra-basic math works pretty well. Guess I still need to work out the precision limit somehow to see what happens to it, and of course to avoid any kind  of over/underflow of the presentation of a float.
    
    ? Probably need some text beyond the statement of each step? Or is there a hilariousness about the correspondence between a "wizard" step and a race step? Such that I don't need the actual process to be about anything? "Almost there"? A generated "almost there" statement from a set of possibilities, which would be kind of a nice reference to the ways these interfaces try to convince us the process is just about finished when we're exhausted and bored?
    
    ? With this I technically have drafts of all the interfaces, with only Danaids feeling genuinely out of shape and unready.

--- js/script.js
+++ js/script.js
@@ -28,8 +28,9 @@ $(document).ready(function () {
   setupDanaids();
   setupTantalus();
   setupPrometheus();
+  setupZeno();
 
-  // $('#prometheus-dialog').dialog('open');
+  // $('#zeno-dialog').dialog('open');
 
 });
 
@@ -317,4 +318,68 @@ function setupPrometheus() {
   });
   // Get rid of the 'x' button on the menu bar
   $peckDialog.parent().find(".ui-dialog-titlebar-close").hide();
+}
+
+
+
+// Set up the Zeno app
+//
+// In Zeno you're asked to select a food or drink but when you open the
+// menu both options are disabled. Most of this is actually in the HTML.
+function setupZeno() {
+  let step = 1;
+
+  // Set up  the icon
+  let  $zenoIcon = $('#zeno-icon');
+  $zenoIcon.draggable();
+  $zenoIcon.on('dblclick',function() {
+    $zenoDialog.dialog('open');
+  });
+
+  let $zenoDialog = $('#zeno-dialog');
+
+  // Create the dialog to house the "app"
+  $zenoDialog.dialog({
+    closeOnEscape: false,
+    autoOpen: false,
+    // You can specify buttons as an array of objects to get finer-grained control
+    // In this instance I want to assign an id for later reference
+    open: function () {
+      if (step === 1) {
+        $('#zeno-previous').button('disable');
+      }
+      else {
+        $('#zeno-previous').button('enable');
+      }
+    },
+    buttons: [
+      {
+        id: "zeno-previous",
+        text: "< Previous",
+        click: function () {
+          $(this).dialog('close');
+          step -= (2 - step);
+          $('#zeno-step').text(step);
+          setTimeout(function () {
+            $zenoDialog.dialog('open');
+          },500);
+        }
+      },
+      {
+        id: "zeno-next",
+        text: "Next >",
+        click: function () {
+          $(this).dialog('close');
+          step += (2 - step)/2;
+          $('#zeno-step').text(step);
+          setTimeout(function () {
+            $zenoDialog.dialog('open');
+          },500);
+        }
+      }
+    ]
+  });
+
+  // Get rid of the 'x' button on the menu bar
+  $zenoDialog.parent().find(".ui-dialog-titlebar-close").hide();
 }
